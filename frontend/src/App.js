10e3ca06bc4f5779b44e3ed61c8caba75c933023
srija9 <srijanakirakommula@gmail.com>
2021-12-04 00:56:25 +0530

    global_ui card created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,41 @@
+import "./App.css";
+// import CardforInputs from "./components/global_ui/card";
 
-import './App.css';
-
-const App = ()=> {
+const App = () => {
   return (
     <div className="App">
-      
+      {/* <CardforInputs
+        className="four"
+        first="ROLL NO."
+        second="NAME"
+        third="MID-1 GRADING"
+        fourth={
+          <a href="https://c.tenor.com/zyL274BVdbMAAAAC/congratulations-congrats.gif">
+            MID-2 GRADING
+          </a>
+        }
+        iscolor="iscolor"
+      />
+      <br />
+      <CardforInputs
+        className="six"
+        first="SUBJECT"
+        second="PRA TOPIC biggggggggggggggggg"
+        third="STATUS"
+        fourth="SUBMIT BEFORE"
+        fifth="EDIT"
+        sixth="DELETE"
+        iscolor="iscolor"
+      />
+      <br />
+      <CardforInputs
+        className="one"
+        first="1_CSE_D_PPS"
+        iscolor="iscolor"
+      />
+      <br /> */}
     </div>
   );
-}
+};
 
 export default App;
