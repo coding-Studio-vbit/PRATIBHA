d4a7f786a18b690573a9f7094500996cd99941b7
Pranchal15 <pranchal.agarwal@gmail.com>
2021-12-08 00:34:42 +0530

    loginPage styles

--- frontend/src/components/login/loginPage.js
+++ frontend/src/components/login/loginPage.js
@@ -1,39 +1,46 @@
-import React from 'react';
-import Button from '../global_ui/buttons/button';
+import React from "react";
+import Button from "../global_ui/buttons/button";
 import { GoogleAuthProvider } from "firebase/auth";
-import { signInWithPopup} from "firebase/auth";
-import { auth } from '../../firebase';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import './loginPage.css'
-
-
+import { signInWithPopup } from "firebase/auth";
+import { auth } from "../../firebase";
+import "./loginPage.css";
 
 export default function LoginPage() {
-    const SignInWithFirebase=()=>{
-        const provider = new GoogleAuthProvider();
-        signInWithPopup(auth,provider)
-        .then((result)=>{
-            console.log(result.user.email)
-            console.log(result.user)
-        })
-        .catch((error)=>{
-            console.log(error)
-        });
+  const SignInWithFirebase = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user.email);
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div className="loginPage">
+      <div className="logos">
+        <img className="image" src="/Abhyas.jpeg" />
+      </div>
+      <div className="row">
 
-    }
-    return (
-        <div className="loginPage" >
-        <div className="logos">
-        <img className="image" src='/Abhyas.jpeg'/>
-        </div>
+      <div className="student">
+
+      <i className="fas fa-user-circle icons"></i>
+      <Button className="studentButton" onClick={SignInWithFirebase}>
+        Login as Student
+      </Button>
+      </div>
+      <div className="faculty">
+
+      <i className="fas fa-graduation-cap icons "></i>
+
     
-       <div>
-       <AccountCircleIcon/>
-       </div>
-<div>
-</div>
-            <Button className="student" onClick={SignInWithFirebase}>Login as Student</Button>
-    
-        </div>
-    )
+      <Button className="facultyButton" onClick={SignInWithFirebase}>
+        Login as Faculty
+      </Button>
+      </div>
+      </div>
+    </div>
+  );
 }
