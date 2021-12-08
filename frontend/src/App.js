0bc9c4178f25ce891487b701c4eb35e516e85fe9
srija9 <srijanakirakommula@gmail.com>
2021-12-08 19:47:49 +0530

    card changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,9 @@
 import "./App.css";
-// import CardforInputs from "./components/global_ui/card";
 
 const App = () => {
   return (
     <div className="App">
-      {/* <CardforInputs
-        className="four"
-        first="ROLL NO."
-        second="NAME"
-        third="MID-1 GRADING"
-        fourth={
-          <a href="https://c.tenor.com/zyL274BVdbMAAAAC/congratulations-congrats.gif">
-            MID-2 GRADING
-          </a>
-        }
-        iscolor="iscolor"
-      />
-      <br />
-      <CardforInputs
-        className="six"
-        first="SUBJECT"
-        second="PRA TOPIC biggggggggggggggggg"
-        third="STATUS"
-        fourth="SUBMIT BEFORE"
-        fifth="EDIT"
-        sixth="DELETE"
-        iscolor="iscolor"
-      />
-      <br />
-      <CardforInputs
-        className="one"
-        first="1_CSE_D_PPS"
-        iscolor="iscolor"
-      />
-      <br /> */}
+      
     </div>
   );
 };
--- frontend/src/components/global_ui/card.js
+++ frontend/src/components/global_ui/card.js
@@ -2,23 +2,11 @@ import React from "react";
 import "./card.css";
 
 const CardforInputs = ({
-  first,
-  className,
-  iscolor,
-  second,
-  third,
-  fourth,
-  fifth,
-  sixth,
+  text,  
 }) => {
   return (
-    <div className={`card-container-${className} ${iscolor}`}>
-      {first && <p className="grid-item">{first}</p>}
-      {second && <p className="grid-item">{second}</p>}
-      {third && <p className="grid-item">{third}</p>}
-      {fourth && <p className="grid-item">{fourth}</p>}
-      {fifth && <p className="grid-item">{fifth}</p>}
-      {sixth && <p className="grid-item">{sixth}</p>}
+    <div className={`card-container`}>
+      {text && <p>{text}</p>}
     </div>
   );
 };
