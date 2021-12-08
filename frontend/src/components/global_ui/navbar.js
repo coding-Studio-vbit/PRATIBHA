3595624f4ae9616b4f72a1b858abf33dc70a387c
chandrakiran343 <chandra.dkiran@outlook.com>
2021-12-08 20:55:01 +0530

    nav bar

---
+++ frontend/src/components/global_ui/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+        <nav>
+        Class List
+        </nav>
+        </div>
+    );
+}
+ 
+export default Navbar;
