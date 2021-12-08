784e431b5640d56248968d1d683958c6db802d72
chandrakiran343 <chandra.dkiran@outlook.com>
2021-12-08 23:01:37 +0530

    Added navbar

--- frontend/src/components/global_ui/navbar.js
+++ frontend/src/components/global_ui/navbar.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './navbar.css'
 
-const Navbar = () => {
+
+const Navbar = ({title,logout=true}) => {
+    /*logout is enabled by default, give logout={false} to hide it
+    give title='' to rename title in the navbar*/
+
     return (
         <div>
-        <nav>
-        Class List
+        <nav className="nav">
+        <button className="bck">
+        <i1 class="fas fa-arrow-left">
+        </i1></button>
+        {title}
+        {(logout!==false?<button className="btn">
+        <i2 class="fas fa-power-off"></i2>
+        Logout</button>:false)}
         </nav>
         </div>
     );
