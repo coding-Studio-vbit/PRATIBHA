32760ac9e7e8874a988c0727fe99961f9a52b47f
srija9 <srijanakirakommula@gmail.com>
2021-12-09 14:15:17 +0530

    check card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+// import Card from './components/global_ui/card/card';
 
 const App = ()=> {
   return (
     <div className="App">
+
+      {/* <Card text="hello" /> */}
       
     </div>
   );
