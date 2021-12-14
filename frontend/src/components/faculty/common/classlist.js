367534df06d0ac6536b5de7bead2889d502bc507
chandrakiran343 <chandra.dkiran@outlook.com>
2021-12-14 19:16:40 +0530

    Added ClassList screen

---
+++ frontend/src/components/faculty/common/classlist.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Navbar from '../../global_ui/navbar/navbar'
+import Card from '../../global_ui/card/card.js'
+import './classList.css'
+
+
+const classList = () => {
+
+    const classes = [{class:'3_CSE_D_PPS',id:0},{class:'2_IT_A_CN',id:1},{class:'2_IT_A_CN',id:2},{class:'2_IT_A_CN',id:3},{class:'2_IT_A_CN',id:4},{class:'2_IT_A_CN',id:5}]
+    return (<div>
+        <Navbar title='Your Classes' logout={true}/>
+        <div className='div-container'>
+        <div className='cards-container'>
+        {classes.map(c=>
+            c.id<3?<span className='card-container'>
+            <Card onClick={()=>{}} text={c.class} /></span>:false
+        )}
+        
+        
+        </div>
+        <div className='cards-container'>
+        {classes.map(c=>
+            c.id>=3?<span className='card-container'>
+            <Card onClick={()=>{}} text={c.class} /></span>:false
+        )}
+        
+        </div>
+        
+        
+
+        
+        </div>
+        
+        </div>);
+}
+ 
+export default classList;
