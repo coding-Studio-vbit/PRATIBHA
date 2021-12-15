0f8c7fb4d01310406da719cbfd9f13247c3a15a9
Pranchal15 <pranchal.agarwal@gmail.com>
2021-12-15 13:10:43 +0530

    dropdwons in locklist and enroll

--- frontend/src/components/faculty/common/LockListSubjects/lockList.js
+++ frontend/src/components/faculty/common/LockListSubjects/lockList.js
@@ -1,106 +1,84 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Select from "react-select";
-import Button from '../../../global_ui/buttons/button';
+import Button from "../../../global_ui/buttons/button";
 import Navbar from "../../../global_ui/navbar/navbar";
-import './lockList.css';
+import "./lockList.css";
 
+const LockList = () => {
+  const [Course, setCourse] = useState("");
+  const [Year, setYear] = useState("");
+  const [Department, setDepartment] = useState("");
+  const [Section, setSection] = useState("");
+  const [Subject, setSubject] = useState("");
+  const Courses = [
+    { value: "B.Tech", label: "B.Tech" },
+    { value: "M.Tech", label: "M.Tech" },
+    { value: "MBA", label: "MBA" },
+  ];
+  const Years = [
+    { value: "1", label: "1" },
+    { value: "2", label: "2" },
+    { value: "3", label: "3" },
+    { value: "4", label: "4" },
+  ];
+  const Departments = [
+    { value: "CSE", label: "Computer Science & Engineering" },
+    {
+      value: "CSEAIML",
+      label: "CSE(Artificial Intelligence & Machine Learning)",
+    },
+    { value: "CSEDS", label: "CSE(Data Science)" },
+    { value: "CSECS", label: "CSE(Cyber Security)" },
+    { value: "CSBS", label: "Computer Science & Business System" },
+    { value: "ECE", label: "Electronics & Communications Engineering" },
+    { value: "EEE", label: "Electrical & Electronics Engineering" },
+    { value: "CE", label: "Civil Engineering" },
+    { value: "ME", label: "Mechanical Engineering" },
+    { value: "IT", label: "Information Technology" },
+  ];
+  const Sections = [
+    { value: "A", label: "A", link: "CSE" },
+    { value: "B", label: "B", link: "CSE" },
+    { value: "C", label: "C", link: "CSE" },
+    { value: "D", label: "D", link: "CSE" },
+    { value: "A", label: "A", link: "ECE" },
+    { value: "B", label: "B", link: "ECE" },
+    { value: "A", label: "A", link: "EEE" },
+    { value: "A", label: "A", link: "IT" },
+    { value: "B", label: "B", link: "IT" },
+  ];
+  const Subjects = [
+    { value: "PPS", label: "PPS", link: "CSE" },
+    {
+      value: "Software Engineering",
+      label: "Software Engineering",
+      link: "CSE",
+    },
+    { value: "Compiler Design", label: "Compiler Design", link: "CSE" },
+    {
+      value: "Engineering Mechanics",
+      label: "Engineering Mechanics",
+      link: "CE",
+    },
+  ];
 
-
-const LockList=()=> {
-    const [Course, setCourse] = useState('');
-    const [Year, setYear] = useState('');
-    const [Department, setDepartment] = useState('')
-    const [Section, setSection] = useState('')
-    const [Subject, setSubject] = useState('')
-    const Courses =[
-        {value:'B.Tech',label:'B.Tech'},
-        {value:'M.Tech',label:'M.Tech'},
-        {value:'MBA',label:'MBA'}
-    ]
-    const Years = [
-        { value: "1", label: "1" },
-        { value: "2", label: "2" },
-        { value: "3", label: "3" },
-        { value: "4", label: "4" },
-      ];
-      const Departments = [
-        { value: "CSE", label: "Computer Science & Engineering" },
-        {
-          value: "CSEAIML",
-          label: "CSE(Artificial Intelligence & Machine Learning)",
-        },
-        { value: "CSEDS", label: "CSE(Data Science)" },
-        { value: "CSECS", label: "CSE(Cyber Security)" },
-        { value: "CSBS", label: "Computer Science & Business System" },
-        { value: "ECE", label: "Electronics & Communications Engineering" },
-        { value: "EEE", label: "Electrical & Electronics Engineering" },
-        { value: "CE", label: "Civil Engineering" },
-        { value: "ME", label: "Mechanical Engineering" },
-        { value: "IT", label: "Information Technology" },
-      ];
-      const Sections = [
-        { value: "A", label: "A", link: "CSE" },
-        { value: "B", label: "B", link: "CSE" },
-        { value: "C", label: "C", link: "CSE" },
-        { value: "D", label: "D", link: "CSE" },
-        { value: "A", label: "A", link: "ECE" },
-        { value: "B", label: "B", link: "ECE" },
-        { value: "A", label: "A", link: "EEE" },
-        { value: "A", label: "A", link: "IT" },
-        { value: "B", label: "B", link: "IT" },
-      ];
-      const Subjects = [
-        { value: "PPS", label: "PPS", link: "CSE" },
-        {
-          value: "Software Engineering",
-          label: "Software Engineering",
-          link: "CSE",
-        },
-        { value: "Compiler Design", label: "Compiler Design", link: "CSE" },
-        {
-          value: "Engineering Mechanics",
-          label: "Engineering Mechanics",
-          link: "CE",
-        },
-      ];
-    
-    return (
-        <div>
-             <div className="lockList-container">
-        <Navbar title="Class List" logout={false}/>
+  return (
+    <div>
+      <div className="lockList-container">
+        <Navbar title="Class List" logout={false} />
         <p className="instruction">*Add your classes for this semester</p>
         <div className="flex-container">
           <div className="dropdown">
-          <p>COURSE</p>
-          <Select
-          className="year"
-            
-              options={Courses}
-
-          />
+            <p>COURSE</p>
+            <Select className="year" options={Courses} />
             <p>YEAR</p>
-            <Select
-              className="year"
-           
-              options={Years}
-            />
+            <Select className="year" options={Years} />
             <p> DEPARTMENT</p>
-            <Select
-              
-              options={Departments}
-            />
+            <Select options={Departments} />
             <p>SECTION</p>
-            <Select
-             
-              options={Sections}
-
-            />
-          <p>SUBJECT</p>
-            <Select
-            
-              options={Subjects}
-            
-            />
+            <Select options={Sections} />
+            <p>SUBJECT</p>
+            <Select options={Subjects} />
             <Button
               className="add-button"
               width="100"
@@ -110,9 +88,7 @@ const LockList=()=> {
             />
           </div>
           <div className="list-and-done">
-            <div className="list-container">
-             
-            </div>
+            <div className="list-container"></div>
             <Button
               className="done-button normal"
               width="100"
@@ -122,9 +98,9 @@ const LockList=()=> {
           </div>
         </div>
       </div>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default LockList;
 
--- frontend/src/components/student/enrollClass/enroll.js
+++ frontend/src/components/student/enrollClass/enroll.js
@@ -1,114 +1,85 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Select from "react-select";
 import Navbar from "../../global_ui/navbar/navbar";
 import Button from "../../global_ui/buttons/button";
 import "./enroll.css";
 
-class StudentEnroll extends Component {
-  constructor() {
-    super();
-    this.state = {
-      selectedOption0: {},
-      selectedOption1: {},
-      selectedOption2: {},
-      selectedOption3: {},
-    };
-  }
 
-  handleChange0 = (selectedOption0) => {
-    this.setState({ selectedOption0 });
-  };
+export default function StudentEnroll() {
+  const [Course, setCourse] = useState("");
+  const [Year, setYear] = useState("");
+  const [Department, setDepartment] = useState("");
+  const [Section, setSection] = useState("");
+  const[Semester,setSemester]=useState("");
 
-  handleChange1 = (selectedOption1) => {
-    this.setState({ selectedOption1 });
-  };
+  const Courses = [
+    { value: "B.Tech", label: "B.Tech" },
+    { value: "M.Tech", label: "M.Tech" },
+    { value: "MBA", label: "MBA" },
+  ];
+  const Years = [
+    { value: "1", label: "1" },
+    { value: "2", label: "2" },
+    { value: "3", label: "3" },
+    { value: "4", label: "4" },
+  ];
+  const Departments = [
+    { value: "CSE", label: "Computer Science & Engineering" },
+    {
+      value: "CSEAIML",
+      label: "CSE(Artificial Intelligence & Machine Learning)",
+    },
+    { value: "CSEDS", label: "CSE(Data Science)" },
+    { value: "CSECS", label: "CSE(Cyber Security)" },
+    { value: "CSBS", label: "Computer Science & Business System" },
+    { value: "ECE", label: "Electronics & Communications Engineering" },
+    { value: "EEE", label: "Electrical & Electronics Engineering" },
+    { value: "CE", label: "Civil Engineering" },
+    { value: "ME", label: "Mechanical Engineering" },
+    { value: "IT", label: "Information Technology" },
+  ];
+  const Sections = [
+    { value: "A", label: "A", link: "CSE" },
+    { value: "B", label: "B", link: "CSE" },
+    { value: "C", label: "C", link: "CSE" },
+    { value: "D", label: "D", link: "CSE" },
+    { value: "A", label: "A", link: "ECE" },
+    { value: "B", label: "B", link: "ECE" },
+    { value: "A", label: "A", link: "EEE" },
+    { value: "A", label: "A", link: "IT" },
+    { value: "B", label: "B", link: "IT" },
+  ];
 
-  handleChange2 = (selectedOption1) => {
-    this.setState({ selectedOption2: selectedOption1 });
-  };
-  handleChange3 = (selectedOption3) => {
-    this.setState({ selectedOption3 });
-  };
-  render() {
-    const Year = [
-      { value: "1", label: "1" },
-      { value: "2", label: "2" },
-      { value: "3", label: "3" },
-      { value: "4", label: "4" },
-    ];
-    const Department = [
-      { value: "CSE", label: "Computer Science & Engineering" },
-      {
-        value: "CSEAIML",
-        label: "CSE(Artificial Intelligence & Machine Learning)",
-      },
-      { value: "CSEDS", label: "CSE(Data Science)" },
-      { value: "CSECS", label: "CSE(Cyber Security)" },
-      { value: "CSBS", label: "Computer Science & Business System" },
-      { value: "ECE", label: "Electronics & Communications Engineering" },
-      { value: "EEE", label: "Electrical & Electronics Engineering" },
-      { value: "CE", label: "Civil Engineering" },
-      { value: "ME", label: "Mechanical Engineering" },
-      { value: "IT", label: "Information Technology" },
-    ];
+  const Semesters=[
+    { value: "1", label: "1" },
+    { value: "2", label: "2" },
 
-    const Semester = [
-      { value: "1", label: "1" },
-      { value: "2", label: "2" },
-    ];
+  ]
+  
 
-    const Sections = [
-      { value: "A", label: "A", link: "CSE" },
-      { value: "B", label: "B", link: "CSE" },
-      { value: "C", label: "C", link: "CSE" },
-      { value: "D", label: "D", link: "CSE" },
-      { value: "A", label: "A", link: "ECE" },
-      { value: "B", label: "B", link: "ECE" },
-      { value: "A", label: "A", link: "EEE" },
-      { value: "A", label: "A", link: "IT" },
-      { value: "B", label: "B", link: "IT" },
-    ];
 
-    const filteredOptions = Sections.filter(
-      (o) => o.link === this.state.selectedOption1.value
-    );
+ 
     return (
       <div className="enroll-container">
         <Navbar title="Enrollment Screen" logout={false} />
 
         <div className="dropdown">
-          <p>YEAR</p>
-          <Select
-            className="year"
-            value={this.state.selectedOption0}
-            onChange={this.handleChange0}
-            options={Year}
-          />
-          <p> DEPARTMENT</p>
-          <Select
-            value={this.state.selectedOption1}
-            onChange={this.handleChange1}
-            options={Department}
-          />
-          <p>SECTION</p>
-          <Select
-            value={this.state.selectedOption2}
-            onChange={this.handleChange2}
-            options={filteredOptions}
-            isDisabled={filteredOptions.length === 0}
-          />
-          <p>SEMESTER</p>
-          <Select
-            value={this.state.selectedOption3}
-            onChange={this.handleChange3}
-            options={Semester}
-          />
+        <p>COURSE</p>
+            <Select className="year" options={Courses} />
+            <p>YEAR</p>
+            <Select className="year" options={Years} />
+            <p> DEPARTMENT</p>
+            <Select options={Departments} />
+            <p>SECTION</p>
+            <Select options={Sections} />
+            <p>SEMESTER</p>
+            <Select options={Semesters} />
         </div>
 
-        <Button className="button" width="150" height="50" children="Enroll" icon={<i class="fas fa-mouse-pointer"></i>}/>
+        <Button className="button normal" width="150" height="50" children="Enroll"/>
       </div>
     );
   }
-}
 
-export default StudentEnroll;
+
+
