75dae6b4658f077810ae7d5d589f1e3a0da32768
Prashanith <prashanith007@gmail.com>
2021-12-19 15:44:16 +0530

    File Name Conflict Fixed

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../context/authContext';
+import { useAuth } from '../context/AuthContext';
 
 
 export default function Home() {
