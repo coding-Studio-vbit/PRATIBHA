f487c3e77bc61bb531cec467bbffc5222a2ab5f1
srija9 <srijanakirakommula@gmail.com>
2021-12-26 18:33:39 +0530

    merged

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import React from "react";
-=======
 import React, { useContext, useEffect } from "react";
->>>>>>> 2e257f2dc08e6cf7939b74d2cfa46c4f33ab892b
 import "./App.css";
 import { AuthProvider, useAuth } from "./components/context/AuthContext";
 import {
