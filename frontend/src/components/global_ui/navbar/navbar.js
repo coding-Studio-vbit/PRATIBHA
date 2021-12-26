16c8dfb9ee9caf259bd596eac7d97698916301af
Pranchal15 <pranchal.agarwal@gmail.com>
2021-12-26 14:18:46 +0530

    nav back

--- frontend/src/components/global_ui/navbar/navbar.js
+++ frontend/src/components/global_ui/navbar/navbar.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './navbar.css'
 
 
-const Navbar = ({title,logout=false,pra = false}) => {
+const Navbar = ({title,back=true,logout=false,pra = false}) => {
     /*logout, pra is disabled by default, give logout={true}/pra={true} to use it
     give title='' to rename title in the navbar*/
 
     return (
         <div>
         <nav className="nav">
-        <button className="bck">
+        {back &&   (  <button className="bck">
         <i className="fas fa-arrow-left">
-        </i></button>
+        </i></button>)}
+    
         <span className='title' >{title}</span>
         {(logout===true?<button className="btn">
         <i className="fas fa-power-off"></i>
