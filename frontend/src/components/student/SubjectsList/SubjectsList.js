8adc725514bc94638a2a2406ed202bf5e8dab177
srija9 <srijanakirakommula@gmail.com>
2022-01-03 01:19:24 +0530

    subjectlist converted table to cards

--- frontend/src/components/student/SubjectsList/SubjectsList.js
+++ frontend/src/components/student/SubjectsList/SubjectsList.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "../../global_ui/navbar/navbar";
-import "../../faculty/generalFaculty/ListOfStudents/ListOfStudents.css";
 import EditIcon from "@mui/icons-material/Edit";
 import { Spinner } from "../../global_ui/spinner/spinner";
 import { db } from "../../../firebase";
-import { doc, getDoc,Timestamp } from "firebase/firestore";
+import { doc, getDoc, Timestamp } from "firebase/firestore";
 import { getStudentData } from "../services/studentServices";
 import { useAuth } from "./../../context/AuthContext";
 import { useLocation, useNavigate } from "react-router-dom";
-import {LoadingScreen} from '../../global_ui/spinner/spinner'
+import { LoadingScreen } from "../../global_ui/spinner/spinner";
+import Card_ from "../../global_ui/card/card_";
+import "./SubjectlistStyles.css";
 
 const SubjectsList = () => {
   const [data, setData] = useState([]);
@@ -27,7 +28,10 @@ const SubjectsList = () => {
     const adminRef = doc(db, "adminData", "coeDeadline");
     const adminDoc = await getDoc(adminRef);
     if (adminDoc.exists()) {
-      let date = new Timestamp(adminDoc.data()["coeDeadline"]["seconds"],adminDoc.data()["coeDeadline"]["nanoseconds"]).toDate();
+      let date = new Timestamp(
+        adminDoc.data()["coeDeadline"]["seconds"],
+        adminDoc.data()["coeDeadline"]["nanoseconds"]
+      ).toDate();
       return date;
     }
   };
@@ -47,7 +51,7 @@ const SubjectsList = () => {
       setCourseTitle(course);
 
       let coedeadLine = await fetchDeadline();
-      
+
       await fetchsubject(document, coedeadLine, course);
     } else {
       setError(error);
@@ -62,38 +66,46 @@ const SubjectsList = () => {
       if (subjectDoc.exists()) {
         const res = subjectDoc.data()["subjects"];
         await res.map(async (item, index) => {
-       
-          
-          let  date1 = new Timestamp(item['deadline1'].seconds,item['deadline1'].nanoseconds).toDate();
+          let date1 = new Timestamp(
+            item["deadline1"].seconds,
+            item["deadline1"].nanoseconds
+          ).toDate();
           console.log(date1);
+          console.log(coedeadLine);
+          let newDate = new Date();
+          let currentDate = newDate.toLocaleDateString("en-GB");
+
           if (coedeadLine > date1) {
             console.log("1");
             mid = 1;
 
-            date=date1.toLocaleDateString('en-GB');
+            date = date1.toLocaleDateString("en-GB");
           } else {
-          
             mid = 2;
-            if(item["deadline2"])
-            {
-
-
-              let  date2 = new Timestamp(item['deadline2'].seconds,item['deadline2'].nanoseconds).toDate();
-               date=date2.toLocaleDateString('en-GB');
-          } 
-            
-            
+            if (item["deadline2"]) {
+              let date2 = new Timestamp(
+                item["deadline2"].seconds,
+                item["deadline2"].nanoseconds
+              ).toDate();
+              date = date2.toLocaleDateString("en-GB");
+            }
           }
-          
+
           console.log(date);
-        // let  date = new Timestamp(deadline.seconds,deadline.nanoseconds).toDate(); 
-        //   let date = new Date(seconds * 1000);
-        //   deadline =
-        //     date.getDate().toString() +
-        //     "-" +
-        //     (date.getMonth() + 1).toString() +
-        //     "-" +
-        //     date.getFullYear().toString();
+          console.log(currentDate);
+
+          // var Difference_In_Time = date.getTime() - currentDate.getTime();
+          // var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+          // console.log(Difference_In_Days);
+
+          // let  date = new Timestamp(deadline.seconds,deadline.nanoseconds).toDate();
+          //   let date = new Date(seconds * 1000);
+          //   deadline =
+          //     date.getDate().toString() +
+          //     "-" +
+          //     (date.getMonth() + 1).toString() +
+          //     "-" +
+          //     date.getFullYear().toString();
 
           await fetchusersubject(document, date, mid, item.subject);
         });
@@ -111,9 +123,7 @@ const SubjectsList = () => {
           let gradetype;
           if (date !== undefined) {
             if (mid === 1) {
-              
               if (item.gradeStatus1 && item.mid_1) {
-               
                 gradetype = "Graded";
               } else if (!item.gradeStatus1 && item.mid_1) {
                 gradetype = "Submitted for Grading";
@@ -147,19 +157,18 @@ const SubjectsList = () => {
     }
   };
 
+
   useEffect(() => {
     fetchdata();
   }, []);
-
-  
 
   return (
     <div>
       <Navbar title={courseTitle} logout={true} />
       {!loading ? (
         error == null ? (
-          <div className="sub_body">
-            <table style={{ marginTop: "4.5rem" }}>
+          <div className="subBody">
+            {/* <table style={{ marginTop: "4.5rem" }}>
               <thead>
                 <tr>
                   <th>SUBJECT</th>
@@ -197,13 +206,36 @@ const SubjectsList = () => {
                     </tr>
                   ))}
               </tbody>
-            </table>
+            </table> */}
+            <div className="list-grid">
+              {data &&
+                data.map((dataitem) => (
+                  <div onClick={()=>{
+                    navigate("/student/uploadPRA", {
+                        state: {
+                          rollno: `${currentUser.email}`,
+                          subject: dataitem.SUBJECT,
+                        },
+                      });
+                  }}>
+                  <Card_
+                    key={dataitem.SUBJECT}
+                    subject={dataitem.SUBJECT}
+                    pra={dataitem.PRA_TOPIC}
+                    status={dataitem.STATUS}
+                    date={dataitem.SUBMIT_BEFORE}
+                  />
+                  </div>
+                ))}
+            </div>
           </div>
         ) : (
           <div>{error}</div>
         )
       ) : (
-        <div><LoadingScreen/></div>
+        <div>
+          <LoadingScreen />
+        </div>
       )}
     </div>
   );
