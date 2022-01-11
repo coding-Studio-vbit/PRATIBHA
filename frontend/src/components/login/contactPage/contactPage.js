0ca00dfd2a4be43176b06184f2923bbbef7fb4e0
Prashanith <prashanith007@gmail.com>
2022-01-11 16:57:53 +0530

    Contact Form Changes

--- frontend/src/components/login/contactPage/contactPage.js
+++ frontend/src/components/login/contactPage/contactPage.js
@@ -33,6 +33,11 @@ function ContactPage() {
     }
 
     function handleSubmit() {
+        setErrors({
+            nameError:null,
+            emailError:null,
+            queryError:null
+        });
         validate();
         console.log(errors);
         if(errors.nameError==null && errors.emailError==null && errors.queryError==null){
@@ -48,22 +53,35 @@ function ContactPage() {
             <div className='formComponent'>
                 <p className='contactTitle'>For queries Contact Us</p>
                 <div className='field'>
-                    <input className='inputFieldStyle' type="text" onChange={(e)=>setName(e.target.value)} placeholder='Name'></input>
-                    {
+                    <label className='labelStyle'>Name</label>
+                    <br/>
+                    <input 
+                    className={errors.nameError==null?"inputFieldStyle":"errorFieldStyle"} type="text" 
+                    onChange={(e)=>setName(e.target.value)} placeholder='Name'
+                    ></input>
+                    {/* {
                         errors.nameError && <p className='errorField'>{errors.nameError}</p>
-                    }
+                    } */}
                 </div>
+
                 <div className='field'>
-                    <input className='inputFieldStyle' type="email" onChange={(e)=>setEmail(e.target.value)} placeholder='Email'></input>
-                    {
-                        errors.nameError && <p className='errorField'>{errors.nameError}</p>
-                    }
+                    <label className='labelStyle'>Email</label>
+                    <br/>
+
+                    <input className={errors.emailError==null?"inputFieldStyle":"errorFieldStyle"} type="email" onChange={(e)=>setEmail(e.target.value)} placeholder='Email'></input>
+                    {/* {
+                        errors.nameError && <p className='errorField'>{errors.emailError}</p>
+                    } */}
                 </div>
+
                 <div className='field'>
-                    <textarea className='textareaStyle' onChange={(e)=>setQuery(e.target.value)} placeholder='Query'></textarea>
-                    {
-                        errors.nameError && <p className='errorField'>{errors.nameError}</p>
-                    }
+                    <label className='labelStyle'>Query</label>
+                    <br></br>
+
+                    <textarea className={errors.queryError==null?"inputFieldStyle":"errorFieldStyle"} onChange={(e)=>setQuery(e.target.value)} placeholder='Query'></textarea>
+                    {/* {
+                        errors.nameError && <p className='errorField'>{errors.queryError}</p>
+                    } */}
                 </div>
 
                 <button className='contactButton' type="submit" onClick={()=>handleSubmit()}>SUBMIT</button>
