1ebac370c4f5dc08db34208bfaac5f51a91d5aaa
chandrakiran343 <chandra.dkiran@outlook.com>
2022-01-12 19:20:27 +0530

    Finished with the HODclasslist screen

--- frontend/src/components/faculty/hod/classListHod.js
+++ frontend/src/components/faculty/hod/classListHod.js
@@ -5,8 +5,11 @@ import Card from '../../global_ui/card/card.js'
 import Button from '../../global_ui/buttons/button'
 import './classListHod.css';
 import { useAuth } from '../../context/AuthContext.js';
+import { getSubjects } from '../services/facultyServices';
+import { fetchDepartments } from '../../student/services/studentServices.js';
 import { fetchSectionsAndSubs } from '../services/facultyServices.js';
 import { useNavigate } from 'react-router-dom';
+import {LoadingScreen} from '../../global_ui/spinner/spinner'
 
 
 
@@ -17,6 +20,8 @@ const HODClassList = () => {
     const {currentUser} = useAuth()
     const [Section, setSection] = useState("");
     const [Subject, setSubject] = useState("");
+    const [subs, setSubs] = useState();
+    const [loading, setLoading] = useState(true);
     const nav = useNavigate()
     const [klass,setKlass] = useState(
       [{ value: "Loading", label: "Loading" }],
@@ -35,6 +40,21 @@ const HODClassList = () => {
       [{ value: "Loading", label: "Loading" }],
 
     )
+    useEffect(() => {
+      const fetchSubjects = async () => {
+        const res = await getSubjects(currentUser.email);
+        console.log(res);
+        if (res === -1) {
+          //display error
+        } else {
+          setSubs(res);
+          setLoading(false);
+        }
+      };
+      fetchSubjects();
+    }, []);
+    console.log(currentUser)
+
     useEffect(()=>{
       let klasses = []
       let departments = []
@@ -47,7 +67,9 @@ const HODClassList = () => {
       setDepartment(departments)
 
 
+
     },[])
+    console.log(klass)
 
     const BTechClasses = [
       "2_CSM_B_Software Engineering",
@@ -89,132 +111,135 @@ const HODClassList = () => {
   function handleCard(){
     
   }
-    return (  
-        <div className='root-hod'>
-        <Navbar style={{marginBottom:'30px'}} title={"HOD"} back = {false} logout={true} />
-        <p className="dep-title">Your Classes</p>
-        <div className="div-container-classesHOD">
-       
-        {BTechClasses.length !== 0 && (
-          <div>
-            <h4> B.Tech</h4>
-            <div className="card-flex">
-              {BTechClasses.map((item) => {
-                return (
-                  <Card
-                    classname="card-container"
-                    onclick={handleCard}
-                    text={item}
-                  />
-                );
-              })}
+    return (loading?
+        <LoadingScreen />:(  
+          <div className='root-hod'>
+          <Navbar style={{marginBottom:'30px'}} title={"HOD"} back = {false} logout={true} />
+          <p className="dep-title">Your Classes</p>
+          <div className="div-container-classesHOD">
+         
+          {subs.btechSubs.length !== 0 && (
+            <div>
+              <h4> B.Tech</h4>
+              <div className="card-flex">
+                {subs.btechSubs.map((item) => {
+                  return (
+                    <Card
+                      classname="card-container"
+                      onclick={handleCard}
+                      text={item.split('BTech_')}
+                      subText={subs.praSetSubs[item]?true:false}
+                    />
+                  );
+                })}
+              </div>
             </div>
-          </div>
-        )}
-        {MTechClasses.length !== 0 && (
-          <div>
-            <h4> M.Tech</h4>
-            <div className="card-flex">
-              {MTechClasses.map((item) => {
-                return (
-                  <Card
-                    classname="card-container"
-                    onclick={handleCard}
-                    text={item}
-                  />
-                );
-              })}
+          )}
+          {subs.mtechSubs.length !== 0 && (
+            <div>
+              <h4> M.Tech</h4>
+              <div className="card-flex">
+                {subs.mtechSubs.map((item) => {
+                  return (
+                    <Card
+                      classname="card-container"
+                      onclick={handleCard}
+                      text={item.split('MTech_')}
+                      subText={subs.praSetSubs[item]?true:false}
+                    />
+                  );
+                })}
+              </div>
             </div>
-          </div>
-        )}
-        {MBAClasses.length !== 0 && (
-          <div>
-            <h4>MBA</h4>
-            <div className="card-flex">
-              {MBAClasses.map((item) => {
-                return (
-                  <Card
-                    classname="card-container"
-                    onclick={handleCard}
-                    text={item}
-                  />
-                );
-              })}
+          )}
+          {subs.mbaSubs.length !== 0 && (
+            <div>
+              <h4>MBA</h4>
+              <div className="card-flex">
+                {subs.mbaSubs.map((item) => {
+                  return (
+                    <Card
+                      classname="card-container"
+                      onclick={handleCard}
+                      text={item.split('MBA_')}
+                      subText={subs.praSetSubs[item]?true:false}
+                    />
+                  );
+                })}
+              </div>
             </div>
+          )}
+        </div>
+          <p className="dep-title">View Department Grades</p>
+          <div className="hod-dd">
+          <div className='xyz'>
+          <span className='dd-text'>Course</span>
+          <Select
+                placeholder=""
+                className="course"
+                options={klass}
+                onChange={(selectedCourse) => {
+                  setCourse(selectedCourse);
+                }}
+              />
           </div>
-        )}
-      </div>
-        <p className="dep-title">View Department Grades</p>
-        <div className="hod-dd">
-        <div className='xyz'>
-        <span className='dd-text'>Course</span>
-        <Select
-              placeholder=""
-              className="course"
-              options={klass}
-              onChange={(selectedCourse) => {
-                setCourse(selectedCourse);
-              }}
-            />
-        </div>
-        <div className='xyz'>
-        <span className='dd-text'>Year</span>
-        <Select
-              placeholder=""
-              className="year"
-              options={Course.value[0]==='M'?MYears: (currentUser.isFirstYearHOD)?[Years[0]]:[Years[1],Years[2],Years[3]]}
-              isDisabled={!Course}
-              onChange={ async (selectedYear)  => {
-                setYear(selectedYear);
-                const res  = await fetchSectionsAndSubs(Course.value,selectedYear.value,department)
-                setSubjects(res.subjects)
-                setsections(res.sections)
-
-              }}
-            />
-        </div>
-
-        <div className='xyz'>
-        <span className='dd-text'>Department</span>
-        <Select
-              placeholder=""
-              className="department"
-              options={department}
-              isDisabled={!Year}
-              onChange={ async (d)  => {
-                setDep(d);
-                
-
-              }}
-            />
-        </div>
-
-        <div className='xyz'>
-        <span className='dd-text'>Section</span>
-        <Select
-              placeholder=""
-              options={sections[dep.value]}
-              isDisabled={!Year}
-              onChange={(selectedSection) => {
-                setSection(selectedSection);
-              }}
-            />
-        </div>
-        <div className='xyz'>
-        <span className='dd-text'>Subject</span>
-        <Select
-              placeholder=""
-              options={subjects[dep.value]}
-            isDisabled={!Section}
-              onChange={(selectedSubject) => {
-                setSubject(selectedSubject);
-                setButton(false)
-              }}/></div>
-        </div>
-        <span className="view-style"><Button icon={<i class="fas fa-search"></i>}className='normal hod-button' disabled={button}  onClick={handleClick} children='View' />
-        </span>
-        </div>
-    );
+          <div className='xyz'>
+          <span className='dd-text'>Year</span>
+          <Select
+                placeholder=""
+                className="year"
+                options={Course.value[0]==='M'?MYears: (currentUser.isFirstYearHOD)?[Years[0]]:[Years[1],Years[2],Years[3]]}
+                isDisabled={!Course}
+                onChange={ async (selectedYear)  => {
+                  setYear(selectedYear);
+                  const res  = await fetchSectionsAndSubs(Course.value,selectedYear.value,department)
+                  setSubjects(res.subjects)
+                  setsections(res.sections)
+  
+                }}
+              />
+          </div>
+  
+          <div className='xyz'>
+          <span className='dd-text'>Department</span>
+          <Select
+                placeholder=""
+                className="department"
+                options={department}
+                isDisabled={!Year}
+                onChange={ async (d)  => {
+                  setDep(d);
+                  
+  
+                }}
+              />
+          </div>
+  
+          <div className='xyz'>
+          <span className='dd-text'>Section</span>
+          <Select
+                placeholder=""
+                options={sections[dep.value]}
+                isDisabled={!Year}
+                onChange={(selectedSection) => {
+                  setSection(selectedSection);
+                }}
+              />
+          </div>
+          <div className='xyz'>
+          <span className='dd-text'>Subject</span>
+          <Select
+                placeholder=""
+                options={subjects[dep.value]}
+              isDisabled={!Section}
+                onChange={(selectedSubject) => {
+                  setSubject(selectedSubject);
+                  setButton(false)
+                }}/></div>
+          </div>
+          <span className="view-style"><Button icon={<i class="fas fa-search"></i>}className='normal hod-button' disabled={button}  onClick={handleClick} children='View' />
+          </span>
+          </div>))
 }
  
 export default HODClassList;
